b4debac78219775b0ec442d9ffdb81acda44051a
Krishanu Roy <krishanuroygit@gmail.com>
2026-07-05 13:52:13 +0530

    fixes frontend and redis

--- src/main/java/com/codingclub/common/dto/UserDto.java
+++ src/main/java/com/codingclub/common/dto/UserDto.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDto {
+public class UserDto implements Serializable {
     private Long id;
     private String githubId;
     private String username;
